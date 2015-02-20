092b92a8144439c6ab86a75293b180141dbc7d92
zedany7 <zedany7@hotmail.com>
2015-02-20 15:07:36 +0200

    gggg

--- almany/Controllers/HomeController.cs
+++ almany/Controllers/HomeController.cs
@@ -10,7 +10,10 @@ namespace almany.Controllers
     {
         public ActionResult Index()
         {////////////////
+            //////
+
             return View();
+
         }
 
         public ActionResult About()
